1c28cb73f4bcb68f8e2606235f06fb3da751147c
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-04-08 18:43:30 +0530

    admin login done

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -7,15 +7,16 @@
 <body class=" bg-gray-100 flex items-center justify-center min-h-screen">
     <div class=" bg-white p-8 rounded-2xl  shadow-lg w-full max-w-sm">
     <h2 class="text-2xl text-center text-gray-800 mb-6 ">Admin Login </h2>
-    <form action="" method="post" class="space-y-4">
+    <form action="/admin-login" method="post" class="space-y-4">
+        @csrf
         <div>
             <label for="" class="text-gray-600 mb-1">Admin name</label>
-            <input type="text"placeholder="Enter Admin name"
+            <input type="text"placeholder="Enter Admin name" name="name"
             class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
         </div>
         <div>
             <label for="" class="text-gray-600 mb-1">Password</label>
-            <input type="text"placeholder="Enter Admin password"
+            <input type="password"placeholder="Enter Admin password" name="password"
             class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
 
         </div>
