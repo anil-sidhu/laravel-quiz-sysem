85e13aaaa561fa061100d943cc1ba64beec7c0f4
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-04-09 19:00:53 +0530

    Navbar, Logout, Category

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Admin Dashboard</title>
+    <title>Admin Categories Page</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-    <x-navbar name={{$name}} ></x-navbar>
+    <x-navbar  name={{$name}} ></x-navbar>
 </body>
 </html>
